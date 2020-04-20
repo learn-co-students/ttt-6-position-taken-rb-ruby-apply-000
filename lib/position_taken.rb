81c49de9afcbaadcaf690763a1da1347edc55977
malcolm staso <mstaso1@gmail.com>
2020-04-20 17:20:14 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,21 @@
 # code your #position_taken? method here!
+
+
+
+#def position_taken? (board, index_number)
+ # if board[index_number] == "X" || "O"
+   # true 
+  #elsif board[index_number] == " " || "" || nil
+  #  false
+  #else
+  #  false
+ #end  
+#end
+
+def position_taken? (board, index_number)
+ !(board[index_number] == " " || board[index_number] == "" || board[index_number] == nil)
+end
+
+
+
+
