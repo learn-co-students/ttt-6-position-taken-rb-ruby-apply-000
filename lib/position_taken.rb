51bff95257d30da8a5102855121b2899c8450261
Chase <chaserabenn@gmail.com>
2020-04-21 21:04:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+# def position_taken?(board, index)
+#   if board[index] == "X" || board[index] == "O"
+#     return true
+#   else
+#     return false
+#   end
+# end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
