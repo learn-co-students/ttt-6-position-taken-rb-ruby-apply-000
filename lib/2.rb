10bcb05e7edb3f24a3032673aa582c1a47166fd0
HAN O <a.ocal88@gmail.com>
2020-06-15 20:49:07 +0000

    Done.

---
+++ lib/2.rb
@@ -0,0 +1,9 @@
+
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  elsif board[index] == "" || board[index] == " " || board[index] == nil
+    false
+  end
+end
