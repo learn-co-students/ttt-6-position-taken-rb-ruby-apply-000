cf1ec73aaf00cf1552f42a382e8e08930f207755
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-20 23:11:44 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,29 @@
 # code your #position_taken? method here!
+  # if not, do something else 
+
+
+#board_array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+#def position_taken?(board_array, space_chosen_int)
+  #if board_array[space_chosen_int - 1] == " "
+    #board_array[space_chosen_int -1] = "X"
+  #else 
+   # puts "Please select another move."
+  #end
+#end
+
+def position_taken?(board,index)
+  if board[index] == " " || board[index] =="" || board[index] == nil
+    return false
+  else 
+    return true
+  end
+end
+
+
+
+
+  
+
+
+
