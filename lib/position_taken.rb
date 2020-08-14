c3509e6fe7af881753bd86fce260cb9ce5769ef1
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-14 22:41:14 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,18 @@
 # code your #position_taken? method here!
+
+whateveryouwanttoput_board = [" "," "," "," "," "," "," "," ",""]
+
+def position_taken?(board, index)
+
+! (board[index] ==  " " || board[index] == "" || board[index] == nil) 
+end
+
+puts position_taken?(whateveryouwanttoput_board, 0)
+puts position_taken?(whateveryouwanttoput_board, 1)
+puts position_taken?(whateveryouwanttoput_board, 2)
+puts position_taken?(whateveryouwanttoput_board, 3)
+puts position_taken?(whateveryouwanttoput_board, 4)
+puts position_taken?(whateveryouwanttoput_board, 5)
+puts position_taken?(whateveryouwanttoput_board, 6)
+puts position_taken?(whateveryouwanttoput_board, 7)
+puts position_taken?(whateveryouwanttoput_board, 8)
